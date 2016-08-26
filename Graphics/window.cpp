8a38fe0a387ca0978e2f732ecb5d87e463b60b55
RaskoSmash <bmecklenburg16@gmail.com>
2016-08-25 17:15:03 -0700

    2 triangles

--- Graphics/window.cpp
+++ Graphics/window.cpp
@@ -1,6 +1,20 @@
 #include "gldecs.h"
 #include "window.h"
 #include <cstring>
+#include <cstdio>
+#include <cstdlib>
+
+#ifdef _DEBUG
+void APIENTRY GL_errorCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar *message, const void *userParam)
+{
+	fprintf(stderr, "%s\n", message);
+
+	if (severity == GL_DEBUG_SEVERITY_HIGH)
+	{
+		abort();
+	}
+}
+#endif
 
 bool Window::init(int a_width, int a_height, char * a_title)
 {
@@ -16,6 +30,14 @@ bool Window::init(int a_width, int a_height, char * a_title)
 	glewExperimental = true;
 	glewInit();
 	glClearColor(1.0f, 0.0f, 1.0f, 1.0f);
+
+#ifdef _DEBUG
+	glEnable(GL_DEBUG_OUTPUT);
+	glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+
+	glDebugMessageCallback(GL_errorCallback, 0);
+	glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, 0, true);
+#endif
 	return true;
 }
 
--- TriangleTest/Main.cpp
+++ TriangleTest/Main.cpp
@@ -9,31 +9,44 @@ int main()
 	Window window;
 	window.init();
 
-	Vertex vert[3] = {  {   0, .5f,0,1},
-						{ .5f,-.5f,0,1},
-						{-.5f,-.5f,0,1}};
+	Vertex vert[3] = { { 0,  .5f,0,1, 1,0,0,1 },
+					   { .5f, -.5f,0,1, 0,1,0,1 },
+					   { -.5f, -.5f,0,1, 0,1,1,1 } };
 	unsigned tris[3] = { 0,1,2 };
 
+	Vertex vats[3] = { { 1,  .5f,0,1, 0,1,1,1 },
+					   { .5f, -.5f,0,1, 0,1,0,1 },
+					   { -.5f, -.5f,0,1, 1,0,0,1 } };
+
+	unsigned trees[3] = { 1,2,0 };
+
 	const char vsource[] =
-		"#version 150\n"
-		"in vec4 position;"
-		"void main() {gl_Position = position; }";
+		"#version 330\n"
+		"layout(location = 0)in vec4 position;"
+		"layout(location = 1)in vec4 color;"
+		"out vec4 vColor;"
+		"void main() { vColor = color; gl_Position = position; } ";
 
 	const char fsource[] =
 		"#version 150\n"
+		"in vec4 vColor;"
 		"out vec4 outColor;"
-		"void main() {outColor = vec4(1.0,0.0,0.0,1.0); }";
+		"void main () { outColor = vColor; } ";
 
-	Geometry geo = makeGeometry(vert, 3, tris, 3);
-	Shader shady = makeShader(vsource, fsource);
+	Geometry geo1 = makeGeometry(vert, 3, tris, 3);
+	Shader shady1 = makeShader(vsource, fsource);
+
+	Geometry geo2 = makeGeometry(vats, 3, trees, 3);
+	Shader shady2 = makeShader(vsource, fsource);
 
 	while (window.step())
 	{
-		draw(shady, geo);
+		draw(shady1, geo1);
+		draw(shady2, geo2);
 	}
 
-	freeGeometry(geo);
-	freeShader(shady);
+	freeGeometry(geo1);
+	freeShader(shady1);
 	window.term();
 
 	return 0;
