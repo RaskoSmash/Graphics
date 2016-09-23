5246abdb26fbce3655db27d433f3f19dbcaca76a
RaskoSmash <bmecklenburg16@gmail.com>
2016-09-23 10:25:47 -0700

    Hope it clamps, can't test due to shaders

--- Graphics/camera.cpp
+++ Graphics/camera.cpp
@@ -1,41 +1,44 @@
-#include "input.h"
-#include "timer.h"
-#include "camera.h"
+#include "Input.h"
+#include "Timer.h"
+#include "Camera.h"
 
-void FlyCamera::update(const Input &in, const Timer &t, const float speed)
+void FlyCamera::lookAt(const glm::vec3 &target)
 {
-	glm::vec3 moveDir = glm::vec3(0,0,0);
-	glm::vec3 right = glm::cross(direction, glm::vec3(0, 1, 0));
-	glm::vec3 up = glm::cross(direction, right);
+	//auto mat =
+	//glm::lookAt(position, normalize(target - position), glm::vec3(0, 1, 0));
+	direction = normalize(target - position);
 
-	yaw += in.getMouseAxisHorizontal() * t.getDeltaTime();// *150;
-	pitch += in.getMouseAxisVertical() * t.getDeltaTime();// *150;
 
-	printf("%f\n", yaw);
+}
 
-	direction = glm::vec3(glm::rotate(yaw, glm::vec3(0, 1, 0))[2]);
-	right = glm::cross(direction, glm::vec3(0, 1, 0));
-	direction = glm::mat3(glm::rotate(pitch, right)) * direction;
+void FlyCamera::update(const Input &in, const Timer &t)
+{
 
-	direction.x = sin(yaw);
-	direction.y = -(sinf(yaw) * cosf(pitch));
-	direction.z = -(cosf(yaw) * sinf(pitch));
+	yaw -= in.getMouseAxisHorizontal() / 200.f;
+	pitch -= in.getMouseAxisVertical() / 200.f;
 
-	if (in.getKeyState('W') == Input::DOWN) { moveDir += direction; }
-	if (in.getKeyState('S') == Input::DOWN) { moveDir -= direction; }
-	if (in.getKeyState('A') == Input::DOWN) { moveDir += right; }
-	if (in.getKeyState('D') == Input::DOWN) { moveDir -= right; }
-	if (in.getKeyState('E') == Input::DOWN) { moveDir -= up; }
-	if (in.getKeyState('Q') == Input::DOWN) { moveDir += up; }
+	pitch = glm::clamp<float>(pitch, -glm::pi<float>() / 2 + 0.0001f,
+		glm::pi<float>() / 2 - 0.0001f);
+
+	glm::mat4 &view = transform;
+	view = glm::rotate(yaw, glm::vec3(0, 1, 0));
+	view = glm::rotate(pitch, glm::vec3(view[0])) * view;
+	view = glm::rotate(roll, glm::vec3(view[2])) * view;
+
+	view = glm::eulerAngleYXZ(yaw, pitch, roll);
+	view = glm::translate(-position) * view;
+
+	glm::vec3 moveDir = glm::vec3(0, 0, 0);
+	if (in.getKeyState('W') == Input::DOWN) moveDir += glm::vec3(view[2]);
+	if (in.getKeyState('S') == Input::DOWN) moveDir -= glm::vec3(view[2]);
+	if (in.getKeyState('A') == Input::DOWN) moveDir += glm::vec3(view[0]);
+	if (in.getKeyState('D') == Input::DOWN) moveDir -= glm::vec3(view[0]);
+	if (in.getKeyState('E') == Input::DOWN) moveDir -= glm::vec3(view[1]);
+	if (in.getKeyState('Q') == Input::DOWN) moveDir += glm::vec3(view[1]);
 
 	if (glm::length(moveDir) >= .7f)
 	{
 		moveDir = glm::normalize(moveDir);
 		position += moveDir * t.getDeltaTime() * speed;
 	}
-}
-
-void FlyCamera::lookAt(const glm::vec3 & target)
-{
-	direction = glm::normalize(target - position);
 }
--- TriangleTest/Main.cpp
+++ TriangleTest/Main.cpp
@@ -1,60 +1,103 @@
-#include "crenderutils.h"
 
-//TriangleTest\main.cpp
+
+#include "crenderutils.h"
 #include "window.h"
-#include "vertex.h"
 #include "Gallery.h"
+#include "timer.h"
+#include "input.h"
+#include "vertex.h"
+#include "camera.h"
+
+#include "../dep/include/GLM/glm.hpp"
+#include "../dep/include/GLM/ext.hpp"
+
 int main()
 {
-
+	Window	window;
 	Gallery gallery;
-	Window window;
-	window.init(600,600);
+	Timer	time;
+	Input   input;
 
-	//Vertex vert[3] = { { 0,  .5f,0,1, 1,0,0,1 },
-	//{ .5f, -.5f,0,1, 0,1,0,1 },
-	//{ -.5f, -.5f,0,1, 0,1,1,1 } };
-	//unsigned tris[3] = { 0,1,2 };
-	//Vertex vats[3] = { { 1,  .5f,0,1, 0,1,1,1 },
-	//{ .5f, -.5f,0,1, 0,1,0,1 },
-	//{ -.5f, -.5f,0,1, 1,0,0,1 } };
-	//unsigned trees[3] = { 1,2,0 };
-	//const char vsource[] =
-	//	"#version 330\n"
-	//	"layout(location = 0)in vec4 position;"
-	//	"layout(location = 1)in vec4 color;"
-	//	"out vec4 vColor;"
-	//	"void main() { vColor = color; gl_Position = position; } ";
-	//const char fsource[] =
-	//	"#version 150\n"
-	//	"in vec4 vColor;"
-	//	"out vec4 outColor;"
-	//	"void main () { outColor = vec4(1,0,0,1) - vColor; } ";
-	//Geometry geo3 = makeGeometry(vert, 3, tris, 3);
-	//Shader shady3 = makeShader(vsource, fsource);
-	//Geometry geo1 = loadObj("../res/models/sphere.obj");
-	//Shader shady1 = loadShader("../res/shaders/simpleVert.txt","../res/shaders/simpleFrag.txt");
-	////Geometry geo2 = makeGeometry(vats, 3, trees, 3);
-	////Shader shady2 = makeShader(vsource, fsource);
-
-
+	window.init(1280, 720);
 	gallery.init();
+	input.init(window);
+	time.init();
 
-	float time = 0;
+	Vertex verts[] = { { 1,1,0,1 },{ 1,-1,0,1 },{ -1,-1,0,1 },{ -1,1,0,1 } };
+
+	unsigned tris[] = { 0,1,2, 2,3,0 };
+
+
+	gallery.loadShader("CAMERA", "../res/shaders/cameraVert.txt",
+		"../res/shaders/cameraFrag.txt");
+
+	gallery.makeObject("quad", verts, 4, tris, 6);
+	gallery.loadObjectOBJ("sphere", "../res/models/sphere.obj");
+
+	gallery.loadObjectOBJ("cube", "../res/models/cube.obj");
+
+	float IDENTITY[16] = { 1,0,0,0,
+		0,1,0,0,
+		0,0,1,0,
+		0,0,0,1 };
+
+	glm::mat4 proj, view, model, model2, model3;
+
+
+	//proj = glm::ortho<float>(-10, 10, -10, 10, -10, 10);
+
+	proj = glm::perspective(45.f, 1.f, 1.f, 50.f);
+
+	//proj = glm::perspective(45.f, 1.f, .1f, 10.f);
+	model2 = glm::translate(glm::vec3(1, 0, 1)) * glm::rotate(180.f, glm::vec3(0, -1, 0));
+	float dt = 0;
+
+	model3 = glm::translate(glm::vec3(0, 0, 0))  * glm::rotate(180.f, glm::vec3(0, 1, 0)) * glm::scale(glm::vec3(5, 5, 5));
+
+	float ct = 0;
+
+	FlyCamera cam;
+	cam.jumpTo(glm::vec3(0, 0, -10));
+	cam.lookAt(glm::vec3(0, 0, 0));
+
 
 	while (window.step())
 	{
-		//time is a uniform
-		time += 0.01666667;
-		draw(gallery.getShader("defaultShader"), gallery.getObject("sphere"), time);
-		//draw(shady2, geo2);
+		time.step();
+		input.step();
+
+
+
+
+		ct += time.getDeltaTime();
+		view = cam.getView();
+		proj = cam.getProjection();
+
+		cam.update(input, time);
+
+		model = glm::translate(glm::vec3(0, ct, 0)) *
+			glm::rotate(ct, glm::vec3(0, 1, 0));
+
+		draw(gallery.getShader("CAMERA"), gallery.getObject("sphere"),
+			glm::value_ptr(model2),
+			glm::value_ptr(view),
+			glm::value_ptr(proj), ct);
+
+		draw(gallery.getShader("CAMERA"), gallery.getObject("cube"),
+			glm::value_ptr(model),
+			glm::value_ptr(view),
+			glm::value_ptr(proj), ct);
+
+		draw(gallery.getShader("CAMERA"), gallery.getObject("quad"),
+			glm::value_ptr(model3),
+			glm::value_ptr(view),
+			glm::value_ptr(proj), ct);
 	}
 
-	/*freeGeometry(geo1);
-	freeShader(shady1);*/
+	input.term();
+	time.term();
 	gallery.term();
 	window.term();
-
 	return 0;
 }
 
